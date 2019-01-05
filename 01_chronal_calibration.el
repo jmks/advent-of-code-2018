adb802a4852277ce712ffdd9924a8008d57c06fe
Jason Schweier <jasons@nulogy.com>
2019-01-04 22:55:54 -0500

    Day 1, part 2

--- 01_chronal_calibration.el
+++ 01_chronal_calibration.el
@@ -58,3 +58,50 @@
     (split-string (buffer-string) "\n" t)))
 
 (resulting-frequency (read-puzzle "01_input"))
+
+; --- Part Two ---
+; You notice that the device repeats the same frequency change list over and over. To calibrate the device, you need to
+; find the first frequency it reaches twice.
+
+; For example, using the same list of changes above, the device would loop as follows:
+
+; Current frequency  0, change of +1; resulting frequency  1.
+; Current frequency  1, change of -2; resulting frequency -1.
+; Current frequency -1, change of +3; resulting frequency  2.
+; Current frequency  2, change of +1; resulting frequency  3.
+; (At this point, the device continues from the start of the list.)
+; Current frequency  3, change of +1; resulting frequency  4.
+; Current frequency  4, change of -2; resulting frequency  2, which has already been seen.
+
+; In this example, the first frequency reached twice is 2. Note that your device might need to repeat its list of
+; frequency changes many times before a duplicate frequency is found, and that duplicates might be found while in the
+; middle of processing the list.
+
+; Here are other examples:
+
+; +1, -1 first reaches 0 twice.
+; +3, +3, +4, -2, -4 first reaches 10 twice.
+; -6, +3, +8, +5, -6 first reaches 5 twice.
+; +7, +7, -2, -7, -4 first reaches 14 twice.
+
+; What is the first frequency your device reaches twice?
+
+(defun first-repeated-frequency-change (readings)
+  "Returns the first resulting frequency that is duplicated from cycling though the READINGS"
+  (let
+      ((current-readings (mapcar 'string-to-int readings))
+       (current-resulting-frequency 0)
+       (resulting-frequencies '()))
+    (while (not (memq current-resulting-frequency resulting-frequencies))
+      (setq resulting-frequencies (cons current-resulting-frequency resulting-frequencies))
+      (setq current-resulting-frequency (+ current-resulting-frequency (car current-readings)))
+      (setq current-readings (cdr current-readings))
+      (if (null current-readings)
+          (setq current-readings (mapcar 'string-to-int readings))))
+
+    current-resulting-frequency))
+
+(first-repeated-frequency-change '("+1" "-1")) ; 0
+(first-repeated-frequency-change '("+1" "-2" "+3" "+1")) ; 2
+
+(first-repeated-frequency-change (read-puzzle "01_input"))
