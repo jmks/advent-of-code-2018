f31486986867253994082aede14155e7cfe866a5
Jason Schweier <jasons@nulogy.com>
2019-01-20 20:31:53 -0500

    Fix bug in list/zip

--- library.el
+++ library.el
@@ -1,6 +1,5 @@
 (defun list/replicate (element times)
   "Returns a list of ELEMENT with TIMES elements"
-  (interactive)
   (let ((result '()))
     (while (> times 0)
       (setq result (cons element result)
@@ -18,4 +17,4 @@
 (defun list/zip (list-1 list-2)
   (if (or (null list-1) (null list-2))
       '()
-    (cons (cons (car list-1) (car list-2)) (zip (cdr list-1) (cdr list-2)))))
+    (cons (cons (car list-1) (car list-2)) (list/zip (cdr list-1) (cdr list-2)))))
