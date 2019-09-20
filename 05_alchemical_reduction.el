1d8058cf9b747d46ee5b8010d53a59c633c0e0ad
Jason Schweier <jasons@nulogy.com>
2019-09-20 19:54:21 -0400

    Solve part 2

--- 05_alchemical_reduction.el
+++ 05_alchemical_reduction.el
@@ -36,6 +36,30 @@
 ;; (Note: in this puzzle and others, the input is large;
 ;; if you copy/paste your input, make sure you get the whole thing.)
 
+;; --- Part Two ---
+;; Time to improve the polymer.
+
+;; One of the unit types is causing problems; it's preventing the polymer from
+;; collapsing as much as it should. Your goal is to figure out which unit type
+;; is causing the most problems, remove all instances of it (regardless of
+;; polarity), fully react the remaining polymer, and measure its length.
+
+;; For example, again using the polymer dabAcCaCBAcCcaDA from above:
+
+;; Removing all A/a units produces dbcCCBcCcD.
+;; Fully reacting this polymer produces dbCBcD, which has length 6.
+;; Removing all B/b units produces daAcCaCAcCcaDA.
+;; Fully reacting this polymer produces daCAcaDA, which has length 8.
+;; Removing all C/c units produces dabAaBAaDA.
+;; Fully reacting this polymer produces daDA, which has length 4.
+;; Removing all D/d units produces abAcCaCBAcCcaA.
+;; Fully reacting this polymer produces abCBAc, which has length 6.
+
+;; In this example, removing all C/c units was best, producing the answer 4.
+
+;; What is the length of the shortest polymer you can produce by removing all
+;; units of exactly one type and fully reacting the result?
+
 (defun trigger (polymer)
   "Returns the polymer after the reaction has been triggered"
   (interactive)
@@ -69,3 +93,19 @@
     (s-trim (buffer-string))))
 
 (length (trigger (puzzle-input)))
+
+(defun optimize (polymer)
+  "Optimize polymer by removing a single unit (e.g a/A, b/B, etc),
+removing it from the polymer, and recording its triggered size.
+
+This return polyer that reduces the most."
+  (interactive)
+  (let* ((units (s-split "" "abcdefghijklmnopqrstuvwxyz" t))
+         (polymer-reductions (--map (s-replace (upcase it) "" (s-replace it "" polymer)) units))
+         (triggered-reductions (--map (cons (length (trigger it)) it) polymer-reductions))
+         (shortest-reduced (--min-by (> (car it) (car other)) triggered-reductions)))
+    (car shortest-reduced)))
+
+(eq (optimize polymer) 4)
+
+(message "Part two has minimal reduction of %d" (optimize (puzzle-input)))
