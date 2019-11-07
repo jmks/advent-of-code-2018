5e67e2db04866bc5e81c2a7cadaf0ee47f6402b5
Jason Schweier <jasons@nulogy.com>
2019-11-06 23:47:56 -0500

    Day 7, part 2

--- 07_the_sum_of_its_parts.el
+++ 07_the_sum_of_its_parts.el
@@ -126,7 +126,7 @@ e.g. Step X must be finished before step Q can begin.
 
 (defun deps/next-step (deps)
   "Selects the next step to perform.
-The next step has no dependencies is is the lowest alphabetical character"
+The next step has no dependencies and is the lowest alphabetical character"
   (interactive)
   (let* ((no-deps (--filter (null (cdr it)) deps))
          (steps (-map 'car no-deps))
@@ -163,3 +163,102 @@ The next step has no dependencies is is the lowest alphabetical character"
     (s-join "" (reverse steps))))
 
 (equal (solve (example-input)) "CABDFE")
+
+;; --- Part Two ---
+
+;; As you're about to begin construction, four of the Elves offer to help.
+;; "The sun will set soon; it'll go faster if we work together."
+;; Now, you need to account for multiple people working on steps simultaneously.
+;; If multiple steps are available, workers should still begin them in alphabetical order.
+
+;; Each step takes 60 seconds plus an amount corresponding to its letter: A=1, B=2, C=3, and so on.
+;; So, step A takes 60+1=61 seconds, while step Z takes 60+26=86 seconds.
+;; No time is required between steps.
+
+;; To simplify things for the example, however, suppose you only have help from one Elf (a total of two workers)
+;; and that each step takes 60 fewer seconds (so that step A takes 1 second and step Z takes 26 seconds).
+;; Then, using the same instructions as above, this is how each second would be spent:
+
+;; Second   Worker 1   Worker 2   Done
+;;    0        C          .
+;;    1        C          .
+;;    2        C          .
+;;    3        A          F       C
+;;    4        B          F       CA
+;;    5        B          F       CA
+;;    6        D          F       CAB
+;;    7        D          F       CAB
+;;    8        D          F       CAB
+;;    9        D          .       CABF
+;;   10        E          .       CABFD
+;;   11        E          .       CABFD
+;;   12        E          .       CABFD
+;;   13        E          .       CABFD
+;;   14        E          .       CABFD
+;;   15        .          .       CABFDE
+
+;; Each row represents one second of time.
+;; The Second column identifies how many seconds have passed as of the beginning of that second.
+;; Each worker column shows the step that worker is currently doing (or . if they are idle).
+;; The Done column shows completed steps.
+
+;; Note that the order of the steps has changed; this is because steps now take time to finish
+;; and multiple workers can begin multiple steps simultaneously.
+
+;; In this example, it would take 15 seconds for two workers to complete these steps.
+
+;; With 5 workers and the 60+ second step durations described above, how long will it take to complete all of the steps?
+
+(require 'cl-lib)
+
+(cl-defun plan/step-time (step base-time)
+  (let ((steps (-map 'char-to-string (number-sequence 65 90))))
+    (+ (-elem-index step steps)
+       1
+       base-time)))
+
+(plan/step-time "A" 60)
+(plan/step-time "A" 0)
+(plan/step-time "Z" 60)
+
+(cl-defun deps/ready-steps (deps)
+  "Returns the steps available to be worked on from DEPS.
+The steps have no dependencies and are sorted alphabetically"
+  (interactive)
+  (let* ((no-deps (--filter (null (cdr it)) deps))
+         (steps (-map 'car no-deps))
+         (sorted (cl-sort steps 'string-lessp)))
+    sorted))
+
+(deps/ready-steps (input/build (example-input)))
+
+(cl-defun deps/time (deps num-workers &optional (step-base-time 60))
+  (let ((second 0)
+        (work '())
+        (done '()))
+    (while deps
+      ;; record completed work
+      (--each (--filter (equal second (car it)) work)
+        (setq done (cons (cdr it) done)
+              deps (deps/apply deps (cdr it))))
+
+      ;; free workers that are done
+      (setq work (--reject (equal second (car it)) work))
+
+      ;; assign work to available workers
+      (let* ((ready-steps (deps/ready-steps deps))
+             (steps-in-progress (-map 'cdr work))
+             (unclaimed-steps (cl-set-difference ready-steps steps-in-progress)))
+        (when (and unclaimed-steps
+                   (< (length work) num-workers))
+          (let* ((next-steps (-take (- num-workers (length work)) unclaimed-steps))
+                 (new-work (--map (cons (+ second (plan/step-time it step-base-time)) it) next-steps)))
+            (setq work (-concat work new-work)))))
+
+      ;; tick
+      (setq second (+ second 1)))
+    (- second 1)))
+
+(deps/time (input/build (example-input)) 2 0) ;; 15
+
+(deps/time (input/build (puzzle-input)) 5 60)
