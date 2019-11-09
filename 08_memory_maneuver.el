fcee543be050bb69dc732088f64db6a9e4ce672b
Jason Schweier <jasons@nulogy.com>
2019-11-08 23:17:21 -0500

    Day 8, part 2

--- 08_memory_maneuver.el
+++ 08_memory_maneuver.el
@@ -66,7 +66,7 @@
              (remaining (cdr result)))
         (setq children (cons child-node children)
               rest remaining)))
-    (cons (make-node :child-nodes children
+    (cons (make-node :child-nodes (reverse children)
                      :metadata-entries (-take metadata-entries rest))
           (-drop metadata-entries rest))))
 
@@ -84,3 +84,44 @@
 (sum-metadata (build-tree (read-puzzle (example-input))))
 
 (sum-metadata (build-tree (read-puzzle (puzzle-input))))
+
+;; --- Part Two ---
+
+;; The second check is slightly more complicated: you need to find the value of the root node (A in the example above).
+
+;; The value of a node depends on whether it has child nodes.
+
+;; If a node has no child nodes, its value is the sum of its metadata entries.
+;; So, the value of node B is 10+11+12=33, and the value of node D is 99.
+
+;; However, if a node does have child nodes, the metadata entries become indexes which refer to those child nodes.
+;; A metadata entry of 1 refers to the first child node, 2 to the second, 3 to the third, and so on.
+;; The value of this node is the sum of the values of the child nodes referenced by the metadata entries.
+;; If a referenced child node does not exist, that reference is skipped.
+;; A child node can be referenced multiple time and counts each time it is referenced.
+;; A metadata entry of 0 does not refer to any child node.
+
+;; For example, again using the above nodes:
+
+;; Node C has one metadata entry, 2. Because node C has only one child node, 2 references a child node which does not exist, and so the value of node C is 0.
+;; Node A has three metadata entries: 1, 1, and 2. The 1 references node A's first child node, B, and the 2 references node A's second child node, C.
+;; Because node B has a value of 33 and node C has a value of 0, the value of node A is 33+33+0=66.
+;; So, in this example, the value of the root node is 66.
+
+;; What is the value of the root node?
+
+(cl-defun node-value (node)
+  (if (null (node-child-nodes node))
+      (sum-metadata node)
+    (->> (node-metadata-entries node)
+         (--reject (equal it 0))
+         (-map (lambda (x) (nth (- x 1) (node-child-nodes node))))
+         (--reject (null it))
+         (-map 'node-value)
+         (-reduce '+))))
+
+(node-value (make-node :child-nodes '() :metadata-entries '(1 2 3)))
+(node-value (make-node :child-nodes (list (make-node :child-nodes '() :metadata-entries '(3 5 7))) :metadata-entries '(1)))
+(node-value (build-tree (read-puzzle (example-input))))
+
+(node-value (build-tree (read-puzzle (puzzle-input))))
